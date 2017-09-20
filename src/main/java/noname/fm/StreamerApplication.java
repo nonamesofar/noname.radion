45ea236e73dccd32f2a4bc21412559253c4ece22
Dan Feodot <Dan.Feodot@misys.com>
2017-09-20 17:20:34 +0300

    Try to fix streaming. Add new media player.

--- src/main/java/noname/fm/StreamerApplication.java
+++ src/main/java/noname/fm/StreamerApplication.java
@@ -1,12 +1,20 @@
 package noname.fm;
 
+import java.io.IOException;
+
+import javax.servlet.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.ResourceRegionHttpMessageConverter;
 
 @SpringBootApplication
+@Configuration
 public class StreamerApplication {
 
   public static void main(String[] args) {
@@ -17,5 +25,35 @@ public class StreamerApplication {
     @Bean
     public HttpMessageConverters customConverter() {
         return new HttpMessageConverters(new ResourceRegionHttpMessageConverter());
+    }
+
+    @Configuration
+    static class Config {
+        @Autowired
+        CounterService counterService;
+
+        @Bean
+        Filter counterFilter() {
+            return new Filter() {
+                @Override
+                public void init(FilterConfig filterConfig) throws ServletException {
+                }
+
+                @Override
+                public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+                    try {
+                        counterService.increment("active-http-sessions");
+                        filterChain.doFilter(servletRequest, servletResponse);
+                    } finally {
+                        counterService.decrement("active-http-sessions");
+                    }
+                }
+
+                @Override
+                public void destroy() {
+                }
+            };
+        }
+
     }
 }
--- src/main/java/noname/fm/controller/StreamController.java
+++ src/main/java/noname/fm/controller/StreamController.java
@@ -1,7 +1,6 @@
 package noname.fm.controller;
 
 import java.io.*;
-import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -11,6 +10,7 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import noname.fm.utils.CombinedInputStream;
 import noname.fm.utils.LimitedInputStream;
 import noname.fm.utils.MusicCollection;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +20,6 @@ import org.springframework.core.io.InputStreamSource;
 import org.springframework.core.io.support.ResourceRegion;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRange;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -38,12 +37,9 @@ public class StreamController {
     @Autowired
     MusicCollection mCollection;
 
-    private static int DEFAULT_BUFFER_SIZE = 5000000;
-
-    @RequestMapping(value="listen", produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
+    @RequestMapping(value="listen", produces = "audio/mpeg")
     //test with http://localhost:8080/listen?id=7
-    public  void getTrack(@RequestParam(value="id") int id, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    public void getTrack(@RequestParam(value="id") int id, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
         ServletOutputStream stream = null;
         BufferedInputStream buf = null;
@@ -52,43 +48,21 @@ public class StreamController {
             String filePath = mCollection.getCollection().get( id ).getPathToFile();
 
             stream = response.getOutputStream();
-
             File file = new File( rootPath + "/" + filePath );
 
             response.addHeader( "Content-Type", "audio/mpeg" );
-            response.addHeader( "Content-Disposition", "inline; filename=\"" + filePath + "\"" );
+            response.addHeader( "Content-Disposition", "inline; filename=\"" + filePath+ "\"" );
             response.setContentLength( (int) file.length() );
 
             FileInputStream input = new FileInputStream( file );
             buf = new BufferedInputStream( input );
             int readBytes = 0;
             //read from the file; write to the ServletOutputStream
-
-            //public synchronized int read(byte b[], int off, int len)
-            byte b[] = new byte[DEFAULT_BUFFER_SIZE+1];
-            int pos = 0;
-            int len = 1000;
-            int filelen = (int) file.length();
-            do{
-                readBytes = buf.read(b, 0, DEFAULT_BUFFER_SIZE);
-                if(readBytes!=-1)
-                    stream.write(b);
-                stream.flush();
-                pos += readBytes;
-                //len += 1000;
-
-            }while(readBytes!=-1);
-
-//            while ((readBytes = buf.read()) != -1) {
-//                stream.write( readBytes );
-//            }
-            System.out.println("-------------------------------------I DIDS SOMETHING!!!!----------------");
-        }catch (SocketTimeoutException e){
-            System.out.println( "The socket thing happened! " + e.getMessage() );
+            while ((readBytes = buf.read()) != -1)
+                stream.write( readBytes );
         } catch (IOException ioe) {
             //TODO: this throwrs servlet time out - figure out what to do
-            System.out.println( "This is just an exception! " + ioe.getMessage() );
-            //throw new ServletException( ioe.getMessage() );
+            throw new ServletException( ioe.getMessage() );
         } finally {
             if (stream != null)
                 stream.close();
@@ -110,7 +84,10 @@ public class StreamController {
 
         FileInputStream input = new FileInputStream( file );
 
-        LimitedInputStream lmt = new LimitedInputStream( input, 0, (int) file.length() );
+        LimitedInputStream[] lmt = new LimitedInputStream[1];
+        lmt[0] = new LimitedInputStream( input, 0, (int) file.length() );
+        CombinedInputStream cmb = new CombinedInputStream( lmt );
+
         String range = request.getHeader( HttpHeaders.RANGE);
         List<HttpRange> httpRanges;
         if (range != null) {
@@ -120,7 +97,7 @@ public class StreamController {
             httpRanges = new ArrayList<>();
             httpRanges.add(HttpRange.createByteRange(0));
         }
-        return HttpRange.toResourceRegions(httpRanges, new InputStreamResource(lmt) {
+        return HttpRange.toResourceRegions(httpRanges, new InputStreamResource(cmb) {
             @Override
             public long contentLength() throws IOException {
                 return 1;
